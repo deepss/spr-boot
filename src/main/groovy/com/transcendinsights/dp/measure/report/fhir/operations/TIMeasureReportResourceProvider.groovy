056bcae7d6f680d10b852b4c60ae0d5bfda1ebe0
dxl0190 <dlakshminarasimhaiah@transcendinsights.com>
2017-05-26 16:25:02 -0700

    added operation parameters and calculated YTD dates

--- src/main/groovy/com/transcendinsights/dp/measure/report/fhir/operations/TIMeasureReportResourceProvider.groovy
+++ src/main/groovy/com/transcendinsights/dp/measure/report/fhir/operations/TIMeasureReportResourceProvider.groovy
@@ -4,10 +4,10 @@ import ca.uhn.fhir.jpa.rp.dstu3.MeasureReportResourceProvider
 import ca.uhn.fhir.model.valueset.BundleTypeEnum
 import ca.uhn.fhir.rest.annotation.Operation
 import ca.uhn.fhir.rest.annotation.OperationParam
-import ca.uhn.fhir.rest.annotation.OptionalParam
 import ca.uhn.fhir.rest.server.IBundleProvider
 import com.transcendinsights.dp.measure.report.fhir.operations.search.DateSearchOperationService
 import org.hl7.fhir.dstu3.model.DateType
+import org.hl7.fhir.dstu3.model.StringType
 import org.springframework.beans.factory.annotation.Autowired
 
 import java.text.DateFormat
@@ -21,21 +21,21 @@ class TIMeasureReportResourceProvider extends MeasureReportResourceProvider {
     @Autowired
     DateSearchOperationService dateSearchOperationService
 
-    @Operation(name = '$searchByDate', bundleType = BundleTypeEnum.SEARCHSET)
-    IBundleProvider searchMeasureReportsByDate(@OperationParam(name='searchByDate',
-            type = DateType) DateType theDate ) {
-        dateSearchOperationService.searchByDate(theDate)
-    }
-
     @SuppressWarnings(['SimpleDateFormatMissingLocale'])
     @Operation(name = '$monthlyMeasureReport', bundleType = BundleTypeEnum.SEARCHSET)
-    IBundleProvider searchMonthlyMeasureReports(@OptionalParam(name='date') DateType givenDate ) {
+    IBundleProvider searchMonthlyMeasureReports(
+            @OperationParam(name = 'orgId') StringType orgId,
+            @OperationParam(name = 'measureId') StringType measureId,
+            @OperationParam(name = 'date') DateType givenDate) {
 //        ex : http://localhost:8090/fhir/MeasureReport/$monthlyMeasureReport
+//        add request body as parameters resource with orgId, measureId, date
         DateFormat dateFormat = new SimpleDateFormat('yyyy-MM-dd')
+        def dateParam
         if (givenDate != null) {
-            dateSearchOperationService.searchMonthlyMeasureReports(dateFormat.parse(givenDate.value))
+            dateParam = dateFormat.format(givenDate.value)
         }else {
-            dateSearchOperationService.searchMonthlyMeasureReports(dateFormat.format(new Date()))
+            dateParam = dateFormat.format(new Date())
         }
+        dateSearchOperationService.searchMonthlyMeasureReports(dateParam, orgId.toString(), measureId.toString())
     }
 }
